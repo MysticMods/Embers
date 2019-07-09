1e35c5b4df38284f84e697d2289adb16ec71c197
EpicSquid <epicsquid315@gmail.com>
2019-07-10 09:45:57 +1000

    Setup 1.14.3 workspace

--- src/main/java/epicsquid/embers/capability/EmberCapabilityManager.java
+++
@@ -1,8 +0,0 @@
-package epicsquid.embers.capability;
-
-import net.minecraftforge.common.capabilities.Capability;
-
-public class EmberCapabilityManager {
-
-  public static final Capability<IEmberCapability> CAPABILITY_EMBER = null;
-}
