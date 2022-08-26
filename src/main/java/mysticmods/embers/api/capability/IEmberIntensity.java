265a4d1de10702998e55c310dba3b5754b789dde
EpicSquid <epicsquid315@gmail.com>
2022-08-26 16:37:08 +1000

    Docs are good

--- src/main/java/mysticmods/embers/api/capability/IEmberIntensity.java
+++ src/main/java/mysticmods/embers/api/capability/IEmberIntensity.java
@@ -6,14 +6,33 @@ import net.minecraftforge.common.util.INBTSerializable;
 
 public interface IEmberIntensity extends INBTSerializable<IntTag> {
 
+	/**
+	 * Gets the amount of intensity for this block to operate
+	 * @return The amount of intensity
+	 */
 	int getMinIntensity();
 
+	/**
+	 * Gets the maximum amount of intensity before this block is considered "overheated"
+	 * @return The intensity at which this machine is overheated
+	 */
 	int getMaxIntensity();
 
+	/**
+	 * Gets the current intensity of the machine
+	 * @return The current machine intensity
+	 */
 	int getIntensity();
 
+	/**
+	 * Sets the intensity of the cap
+	 * @param intensity the intensity to set the block to
+	 */
 	void setIntensity(int intensity);
 
+	/**
+	 * Called when the capability is changed
+	 */
 	default void onChange() {
 		// Do nothing, just for blocks to do something when the intensity changes
 	}
