f266a2b5b4b7aa651bff9f34737cb83097089e66
EpicSquid <epicsquid315@gmail.com>
2022-08-31 09:19:15 +1000

    Added the new items

--- src/main/java/mysticmods/embers/init/EmbersItems.java
+++ src/main/java/mysticmods/embers/init/EmbersItems.java
@@ -11,9 +11,13 @@ import net.minecraftforge.common.Tags;
 public class EmbersItems {
 	private static final Registrate REGISTRATE = Embers.registrate();
 	public static final ItemEntry<Item> CAMINITE_BRICK = REGISTRATE.item("caminite_brick", Item::new).register();
+	public static final ItemEntry<Item> CAMINITE_BLEND = REGISTRATE.item("caminite_blend", Item::new).register();
 	public static final ItemEntry<Item> EMBER_CRYSTAL = REGISTRATE.item("ember_crystal", Item::new).register();
 	public static final ItemEntry<Item> CINDERSTEEL_INGOT = REGISTRATE.item("cindersteel_ingot", Item::new).register();
 	public static final ItemEntry<Item> DAWNSTONE_INGOT = REGISTRATE.item("dawnstone_ingot", Item::new).register();
+	public static final ItemEntry<Item> ASHEN_STEEL_INGOT = REGISTRATE.item("ashen_steel_ingot", Item::new).register();
+	public static final ItemEntry<Item> QUICKSILVER = REGISTRATE.item("quicksilver", Item::new).register();
+	public static final ItemEntry<Item> CINNABAR = REGISTRATE.item("cinnabar", Item::new).register();
 
 	public static void init() {
 	}
